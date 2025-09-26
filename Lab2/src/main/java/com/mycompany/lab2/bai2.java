943366988981885999c9d502e8ed099cb89ab473
Kuro <Kuro@Kuro.lan>
2025-09-27 00:14:11 +0700

    Nộp bài Lab 2

--- Lab2/src/main/java/com/mycompany/lab2/bai2.java
+++ Lab2/src/main/java/com/mycompany/lab2/bai2.java
@@ -1,8 +1,9 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.lab2;
+
+import java.util.Scanner;
 
 /**
  *
@@ -11,6 +12,49 @@ package com.mycompany.lab2;
 public class bai2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+        double a, b, c;
+
+        System.out.println("Nhap a = ");
+        a = sc.nextDouble();
+
+        System.out.println("Nhap b = ");
+        b = sc.nextDouble();
+
+        System.out.println("Nhap c = ");
+        c = sc.nextDouble();
+
+        if (a == 0) {
+
+            if (b != 0) {
+                double x = -c / b;
+                System.out.println("Phuong trinh bac nhat co 1 nghiem: x = " + x);
+            } else {
+                if (c == 0) {
+                    System.out.println("Phuong trinh co vo so nghiem");
+                } else {
+                    System.out.println("Phuong trinh vo nghiem");
+                }
+            }
+        } else {
+
+            double delta = b * b - 4 * a * c;
+
+            if (delta < 0) {
+
+                System.out.println("Phuong trinh vo nghiem");
+            } else if (delta == 0) {
+
+                double x = -b / (2 * a);
+                System.out.println("Phuong trinh co nghiem kep: x = " + x);
+            } else {
+
+                double x1 = (-b + Math.sqrt(delta)) / (2 * a);
+                double x2 = (-b - Math.sqrt(delta)) / (2 * a);
+                System.out.println("Phuong trinh co 2 nghiem phan biet:");
+                System.out.println("x1 = " + x1);
+                System.out.println("x2 = " + x2);
+            }
+        }
     }
 }
--- Lab2/src/main/java/com/mycompany/lab2/bai4.java
+++ Lab2/src/main/java/com/mycompany/lab2/bai4.java
@@ -1,8 +1,9 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.lab2;
+
+import java.util.Scanner;
 
 /**
  *
@@ -10,7 +11,43 @@ package com.mycompany.lab2;
  */
 public class bai4 {
 
+    private static String[] args;
+
+    //Tổng hợp chức năng của các demo khác (dự án khác) vào menu mà ko cần viết lại code
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        menu();
+    }
+
+    public static void menu() {
+        Scanner sc = new Scanner(System.in);
+        int cn;
+        System.out.println("      DS CHUC NANG     ");
+        System.out.println("+---------------------+");
+        System.out.println("1.Giai PT bac 1");
+        System.out.println("2.Giai PT bac 2");
+        System.out.println("3.Tien dien");
+        System.out.println("4.Ket thuc");
+        System.out.println("+---------------------+");
+        System.out.println("Xin chon chuc nang mong muon :");
+        cn = sc.nextInt();
+        switch (cn) {
+            case 1:
+                System.out.println("Giai PT bac 1");
+                bai1.main(args);
+                break;
+            case 2:
+                System.out.println("Giai PT bac 2");
+                bai2.main(args);
+                break;
+            case 3:
+                System.out.println("Tien dien");
+                bai3.main(args);
+                break;
+            default:
+                System.out.println("ket thuc chương trinh");
+                break;
+
+        }
+
     }
 }
